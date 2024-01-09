c08d30681867cdeb2e353ae450feae8d8b04164b
Tezz-tech <139757148+Tezz-tech@users.noreply.github.com>
2024-01-09 14:37:35 +0000

    another

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,14 +1,56 @@
+import { Link } from "react-router-dom";
+import img1 from "../IMG/Rectangle 81.png"
+import img2 from "../IMG/Rectangle 86.png"
+import img3 from "../IMG/Rectangle 95.png"
 function Home() {
   return (
-    <div className="precious-nav">
+    <>
+      <div className="precious-one">
         <div className="left">
-            <img src="" alt="" />
-            <h1>We Help You Building The Dreams & Bring More Than You Expect</h1>
-            <button>Explore Properties</button>
+          <h1>
+            We Help You <br /> Building The Dreams & Bring More <br /> Than You
+            Expect
+          </h1>
+          <Link className="fancy">
+            <span class="top-key"></span>
+            <span class="text">Search Properties</span>
+            <span class="bottom-key-1"></span>
+            <span class="bottom-key-2"></span>
+          </Link>
         </div>
-        <div className="right">
+        <div className="right"></div>
+      </div>
+      <div className="precious-two">
+        <div className="card">
+          <img src={img1} alt="" />
+          <h3>Lodha Group</h3>
+          <p>35+ years of experience</p>
+          <span>270+ Project Done</span><br />
+          <button>Check Profile</button>
         </div>
-    </div>
+        <div className="card">
+          <img src={img2} alt="" />
+          <h3>Lodha Group</h3>
+          <p>35+ years of experience</p>
+          <span>270+ Project Done</span><br />
+          <button>Check Profile</button>
+        </div>
+        <div className="card">
+          <img src={img3} alt="" />
+          <h3>Lodha Group</h3>
+          <p>35+ years of experience</p>
+          <span>270+ Project Done</span><br />
+          <button>Check Profile</button>
+        </div>
+        <div className="card">
+          <img src={img1} alt="" />
+          <h3>Lodha Group</h3>
+          <p>35+ years of experience</p>
+          <span>270+ Project Done</span><br />
+          <button>Check Profile</button>
+        </div>
+      </div>
+    </>
   );
 }
 export default Home;
