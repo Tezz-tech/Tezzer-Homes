5cb28895ad9119686e9c2a398afb8e9c61c27bd0
Tezz-tech <139757148+Tezz-tech@users.noreply.github.com>
2024-01-09 08:50:15 +0000

    creator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import DevesLand from './Pages/DevesLand';
+import DevelopersDesc from './Pages/DevelopersDesc'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/DevelopersPage' element={<DevesLand/>}/>
+                <Route path='/DevelopersDescription' element={<DevelopersDesc/>}/>
+                
+            </Routes>
+        </BrowserRouter>
     </div>
   );
 }
